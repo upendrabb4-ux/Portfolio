2720dc5c0bcfc364ee14d27f459a70a6d9419def
Upendra B <upendrabb4@gmail.com>
2026-09-25 14:20:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/resume")
+def resume():
+    return send_from_directory(
+        os.path.join(app.root_path, "static", "resume"),
+        "resume.pdf"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
